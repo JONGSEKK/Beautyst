d31cab1e5395a6067b1b13e1b93701a72f261be5
azrishrf <ishrafazri@gmail.com>
2022-12-19 23:00:53 +0800

    update azri

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,3 +1,15 @@
-function close() {
-    document.getElementById("myNavbar").style.display = "none";
-}
+// function close() {
+//     document.getElementById("myNavbar").style.display = "none";
+// }
+
+let isOpen = true;
+
+document.querySelector(".list-icon").addEventListener("click", () => {
+    if (isOpen) {
+        document.getElementById("myNavbar").style.display = "none";
+        isOpen = false;
+    } else {
+        document.getElementById("myNavbar").style.display = "block";
+        isOpen = true;
+    }
+});
